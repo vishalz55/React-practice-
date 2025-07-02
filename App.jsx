5997eac1e2613f69e9d5c58d550ae2b1d7108cb6
Vishal Mungal <166546124+vishalz55@users.noreply.github.com>
2025-07-02 23:16:00 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import Formtwo from './Formtwo.jsx'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Formtwo></Formtwo>
+    </div>
+  )
+}
+
+export default App
